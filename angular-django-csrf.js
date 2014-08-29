34471874ff43fec1828fc61602d5e28a8e40e38e
matiboy <mathieu.chauvinc@gmail.com>
2014-08-29 14:07:06 +0800

    Initial commit

---
+++ angular-django-csrf.js
@@ -0,0 +1,27 @@
+angular.module('angularDjangoCsrf', ['ngCookies']).provider('djangoCsrf',[function(){
+  var headerName = 'X-CSRFToken';
+  var cookieName = 'csrftoken';
+  var allowedMethods = ['GET'];
+
+  this.setHeaderName = function(n) {
+    headerName = n;
+  }
+  this.setCookieName = function(n) {
+    cookieName = n;
+  }
+  this.setAllowedMethods = function(n) {
+    allowedMethods = n;
+  }
+  this.$get = ['$cookies', function($cookies){
+    return {
+      'request': function(config) {
+        if(allowedMethods.indexOf(config.method) === -1) {
+          // do something on success
+          config.headers[headerName] = $cookies[cookieName];
+          return config;
+        }
+      }
+    }
+  }];
+}]);
+
